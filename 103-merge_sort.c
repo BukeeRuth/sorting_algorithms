1d48800ffa13d6542aa0869cc492a6da46bf5f10
BukeeRuth <olatunjibukola018@gmail.com>
2023-12-17 16:27:56 -0800

    merge sort

---
+++ 103-merge_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+/**
+ * merge_subarr - Merge two subarrays into a single sorted subarray.
+ * @subarr: The original array containing the two subarrays to merge.
+ * @buff: A buffer to store the sorted subarray.
+ * @front: The starting index of the first subarray.
+ * @mid: The ending index of the first subarray and the starting index of the second subarray.
+ * @back: The ending index of the second subarray.
+ */
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back)
+{
+	size_t i, j, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
+
+	printf("[Done]: ");
+	print_array(subarr + front, back - front);
+}
+
+/**
+ * merge_sort_recursive - Implement the merge sort algorithm through recursion.
+ * @subarr: The subarray of the original array to sort.
+ * @buff: A buffer to store the sorted result.
+ * @front: The starting index of the subarray.
+ * @back: The ending index of the subarray.
+ */
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
+{
+	size_t mid;
+
+	if (back - front > 1)
+	{
+		mid = front + (back - front) / 2;
+		merge_sort_recursive(subarr, buff, front, mid);
+		merge_sort_recursive(subarr, buff, mid, back);
+		merge_subarr(subarr, buff, front, mid, back);
+	}
+}
+
+/**
+ * merge_sort - Sort an array of integers in ascending
+ *              order using the merge sort algorithm.
+ * @array: The array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Implements the top-down merge sort algorithm.
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *buffer;
+
+	if (array == NULL || size < 2)
+		return;
+
+	buffer = malloc(sizeof(int) * size);
+	if (buffer == NULL)
+		return;
+
+	merge_sort_recursive(array, buffer, 0, size);
+
+	free(buffer);
+}
+
